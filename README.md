c5bfc484ad3ac3db41be38288476cbb520caa613
fiftysnd <llgaman008@gmail.com>
2024-10-04 18:23:30 +0700

    tugas praktikum api php jadi

---
+++ README.md
@@ -0,0 +1,17 @@
+PRAKTIKUM API PHP
+![php](image.png) pertama membuat code php seperti digambar
+![data html](image-1.png) lalu menambahkan kode html untuk menampilkan data dalam table dan juga menambahkan sedikit css didalamnya
+![index.php](image-4.png) hasil setelah semua selesai akan seperti ini
+
+setelah itu mencoba untuk method menggunakan JS ![JS](image-2.png) agar berhasil, sebelumnya harus menginstall axios dan express
+![index.js](image-3.png) ini untuk tampilan dari js
+
+TUGAS PRAKIKUM API
+![nomor 1](image-5.png) disini saya menambahkan beberapa <th> yaitu nama dan email
+![tampilan](image-6.png) lalu sistem akan menampilkan email yang tercantum
+
+![nomor 2](image-7.png) untuk penggunaan filter sedikit ditambahkan kode php dan html seperti digambar
+![tampilan](image-8.png) setelah selesai maka akan seperti ini dan filter bisa menggunakan nama atau id, dan juga keduanya
+![filter](image-9.png)
+
+![mengubah css](image-10.png)menambahkan sedikit kode css agar terlihat menarik
---
+++ tampilan.php
@@ -0,0 +1,64 @@
+<?php
+$url= "https://jsonplaceholder.typicode.com/comments";
+$response = file_get_contents($url);
+$data = json_decode($response, true);
+
+// Initialize filter variables
+$filter_id = '';
+$filter_name = '';
+
+// Check if the filter form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $filter_id = isset($_POST['filter_id']) ? intval($_POST['filter_id']) : '';
+    $filter_name = isset($_POST['filter_name']) ? $_POST['filter_name'] : '';
+}
+
+// Filter data based on the input
+$filtered_data = array_filter($data, function($comment) use ($filter_id, $filter_name) {
+    return (!$filter_id || $comment['id'] == $filter_id) &&
+           (!$filter_name || stripos($comment['name'], $filter_name) !== false);
+});
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Comments (PHP)</title>
+    <link rel="stylesheet" href="tampilan.css">
+</head>
+<body>
+    <h1>Data Comments dari JSONPlaceholder API (PHP)</h1>
+
+    <form method="POST" action="">
+        <label for="filter_id">Filter by ID:</label>
+        <input type="number" name="filter_id" id="filter_id" value="<?php echo htmlspecialchars($filter_id); ?>">
+
+        <label for="filter_name">Filter by Name:</label>
+        <input type="text" name="filter_name" id="filter_name" value="<?php echo htmlspecialchars($filter_name); ?>">
+
+        <input type="submit" value="Filter">
+    </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Body</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($filtered_data as $comment): ?>
+                <tr>
+                    <td><?php echo $comment['id']; ?></td>
+                    <td><?php echo htmlspecialchars($comment['name']); ?></td>
+                    <td><?php echo htmlspecialchars($comment['email']); ?></td>
+                    <td><?php echo htmlspecialchars($comment['body']); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
